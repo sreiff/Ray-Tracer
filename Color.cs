415bf87f49a8ca644a35e4bdbce96a413d38045f
Jonathan <kolb@ccs.neu.edu>
2012-03-29 13:07:10 -0400

    Adding Vector and Color classes and requiered math

---
+++ Color.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace renderer
+{
+    public class Color
+    {
+        public double R;
+        public double G;
+        public double B;
+
+        public Color(double r, double g, double b) { R = r; G = g; B = b; }
+
+        public static Color Times(double n, Color v)
+        {
+            return new Color(n * v.R, n * v.G, n * v.B);
+        }
+        public static Color Times(Color v1, Color v2)
+        {
+            return new Color(v1.R * v2.R, v1.G * v2.G, v1.B * v2.B);
+        }
+
+        public static Color Plus(Color v1, Color v2)
+        {
+            return new Color(v1.R + v2.R, v1.G + v2.G, v1.B + v2.B);
+        }
+        public static Color Minus(Color v1, Color v2)
+        {
+            return new Color(v1.R - v2.R, v1.G - v2.G, v1.B - v2.B);
+        }
+
+        public static readonly Color Background = new Color(0, 0, 0);
+        public static readonly Color DefaultColor = new Color(0, 0, 0);
+        //====================================================================
+        // Found this online should work.
+        //====================================================================
+        public double Legalize(double d)
+        {
+            return d > 1 ? 1 : d;
+        }
+
+        internal System.Drawing.Color ToDrawingColor()
+        {
+            return System.Drawing.Color.FromArgb((int)(Legalize(R) * 255), (int)(Legalize(G) * 255), (int)(Legalize(B) * 255));
+        }
+        //====================================================================
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace raytracer
+{
+    class Vector
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Vector(double x, double y, double z) { X = x; Y = y; Z = z; }
+
+        public static Vector Times(double n, Vector v)
+        {
+            return new Vector(v.X * n, v.Y * n, v.Z * n);
+        }
+        public static Vector Minus(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+        public static Vector Plus(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+        public static double Dot(Vector v1, Vector v2)
+        {
+            return (v1.X * v2.X) + (v1.Y * v2.Y) + (v1.Z * v2.Z);
+        }
+        public static double Mag(Vector v) { return Math.Sqrt(Dot(v, v)); }
+        public static Vector Norm(Vector v)
+        {
+            double mag = Mag(v);
+            double div = mag == 0 ? double.PositiveInfinity : 1 / mag;
+            return Times(div, v);
+        }
+        public static Vector Cross(Vector v1, Vector v2)
+        {
+            return new Vector(((v1.Y * v2.Z) - (v1.Z * v2.Y)), ((v1.Z * v2.X) - (v1.X * v2.Z)), ((v1.X * v2.Y) - (v1.Y * v2.X)));
+        }
+        public static bool Equals(Vector v1, Vector v2)
+        {
+            return (v1.X == v2.X) && (v1.Y == v2.Y) && (v1.Z == v2.Z);
+        }
+    }
+}
