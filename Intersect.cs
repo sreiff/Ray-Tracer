3532386fec0f90cebadb4a20b541104955cd6788
Jonathan <kolb@ccs.neu.edu>
2012-03-29 13:18:39 -0400

    Adding Intersect object and fixing namespace of Color class

---
+++ Intersect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace raytracer
+{
+    class Intersect
+    {
+        SObject obj;
+        Ray ray;
+        double distance;
+    }
+}
