ff97721597bf4cd2a9f8bfca91fb3bdb71344c9a
David Thor <thor.d@husky.neu.edu>
2012-04-02 15:11:29 -0400

    Added namespace to camera class.

--- Camera.cs
+++ Camera.cs
@@ -1,16 +1,18 @@
-class Camera {
-	public Vector position;
-	public Vector up;
-	public Vector direction;
-	public Vector right;
-
-	public static Camera New(Vector pos, Vector dir) {
-		Camera c = new Camera();
-		c.position 	= pos;
-		c.direction 	= Vector.Norm(Vector.Minus(dir, pos));
-		c.right		= Vector.Times(1.5, Vector.Norm(Vector.Cross(direction, new Vector(0, -1, 0))));
-		c.up		= Vector.Times(1.5, Vector.Norm(Vector.Cross(direction, c.right)));
-
-		return c;
+namespace raytracer {
+	class Camera {
+		public Vector position;
+		public Vector up;
+		public Vector direction;
+		public Vector right;
+	
+		public static Camera New(Vector pos, Vector dir) {
+			Camera c = new Camera();
+			c.position 	= pos;
+			c.direction 	= Vector.Norm(Vector.Minus(dir, pos));
+			c.right		= Vector.Times(1.5, Vector.Norm(Vector.Cross(direction, new Vector(0, -1, 0))));
+			c.up		= Vector.Times(1.5, Vector.Norm(Vector.Cross(direction, c.right)));
+	
+			return c;
+		}
 	}
 }
