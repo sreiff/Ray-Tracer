1f9a365de9da247a6370069851898f4750ea599b
Jonathan <kolb@ccs.neu.edu>
2012-04-04 09:49:39 -0400

    Adding Triangle and Light Source from Sreiff's email

---
+++ LightSource.cs
@@ -0,0 +1,18 @@
+﻿namespace raytracer {
+
+    class LightSource
+    {
+
+        public Vector position;
+
+        public Color color;
+
+        public Vector direction;
+
+
+
+        //will eventually create a ray from this light source
+
+    }
+
+}
---
+++ Triangle.cs
@@ -0,0 +1,33 @@
+﻿namespace raytracer
+{
+
+    class Triangle : SObject
+    {
+
+        public Vector Position1;
+
+        public Vector Position2;
+
+        public Vector Position3;
+
+
+
+        public override Intersect Intersect(Ray ray)
+        {
+
+            //we will write this in class Wednesday
+
+        }
+
+
+
+        public override Vector Normal(Vector pos)
+        {
+
+            //still need to write this
+
+        }
+
+    }
+
+}
